86764147e4e020b930f3f50e83af230c3b0d5a23
ELFMZim <lopesmonteirojoaofelipe@gmail.com>
2026-08-06 00:45:34 +0200

    GET method fixed

--- main.c
+++ main.c
@@ -31,33 +31,29 @@ int main()
     perror("Error listen");
     return -1;
   }
-  struct sockaddr_in peer_addr;
-  socklen_t peer_addr_size = sizeof(peer_addr);
-  int accepted_sock_fd;
-  if((accepted_sock_fd = accept(sock_fd, (struct sockaddr *)&peer_addr, &peer_addr_size)) == -1)  
-  {
-    perror("Error accept");
-    return -1;
-  }
+	
+	struct sockaddr_in peer_addr;
+	socklen_t peer_addr_len = sizeof(peer_addr);
+	int accepted_sock_fd = accept(sock_fd, (struct sockaddr*)&peer_addr, &peer_addr_len);
+	
+	char receive_buf[BUF_SIZE];
+	char write_buf[BUF_SIZE];
 
-  char request[BUF_SIZE];
-  read(accepted_sock_fd, request, sizeof(request));
-  char http_header[20];
-  char http_body[20];
- 
+	recv(accepted_sock_fd, receive_buf, sizeof(receive_buf), 0);
+	printf("%s\n", receive_buf);
+	
+	dprintf(accepted_sock_fd, 
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type:text/html\r\n"
+		"\r\n"
+	);
 
-  // Não tá funcionando, não consegui entender como mandar a response direito. 
-  strncpy(http_header, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=ISO-8859-4\r\nContent-Length: 400\r\n\r\n", sizeof(http_header));
-  strncpy(http_body, "<h1>Hello world </h1>", sizeof(http_body));
-  
-  printf("%li\n", send(accepted_sock_fd, http_header, sizeof(http_header), 0));
-  
-  printf("%li\n", send(accepted_sock_fd, http_body, sizeof(http_body), 0));
-  
-  
+	dprintf(accepted_sock_fd,
+		"<h1> Hello world <h1>"
+	);
 
   close(sock_fd);
-  close(accepted_sock_fd);
+	close(accepted_sock_fd);
   
   return 0;
 }
